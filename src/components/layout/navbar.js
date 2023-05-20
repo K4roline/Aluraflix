b663c9f6815719169d895b5ec0bbc732d2eb83f1
Karoline Correa <ckaroline226@gmail.com>
2023-05-20 00:42:58 -0300

    Desenvolvendo o projeto

--- src/components/layout/navbar.js
+++ src/components/layout/navbar.js
@@ -1,26 +1,26 @@
 import { Link } from "react-router-dom"
 
-import container from "./container"
+import Container from "./container"
 
-import styles from './navbar.modules.css'
+import Styles from './navbar.modules.css'
 import Logo from '../imgs/logo.png'
 
 function Navbar() {
     return (
-        <nav class={styles.navbar}>
-            <container>
+        <nav class={Styles.navbar}>
+            <Container>
                 <Link to="/">
-                    <img src="./components/imgs/logo.png"/>
+                    <img src={Logo}/>
                 </Link>
-                <ul class={styles.list}>
-                    <li class={styles.item}>
+                <ul class={Styles.list}>
+                    <li class={Styles.item}>
                         <Link to="/">Home</Link>
                     </li>
-                    <li class={styles.item}>
+                    <li class={Styles.item}>
                         <Link to="/addVideo">Adicionar Video</Link>
                     </li>
                 </ul>
-            </container>
+            </Container>
         </nav>
     )
 }
